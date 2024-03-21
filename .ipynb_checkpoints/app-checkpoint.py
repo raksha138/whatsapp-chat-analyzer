3f999def5b9dd8e03e7eff31859ae5d1badbad80
Raksha <rakshapatil138@gmail.com>
2024-03-21 20:27:23 +0530

    first commit

---
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import  preprocessor
+
+st.sidebar.title("Whatsapp Chat Analyzer")
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+if uploaded_file is not None:
+  bytes_data = uploaded_file.getvalue()
+  data = bytes_data.decode("utf-8")
+  df = preprocessor.preprocess(data)
+
+  st.dataframe(df)
